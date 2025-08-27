2cd0c145d722877cc267062f3aad6828c53ce735
melz3r <ola.letrud@gmail.com>
2025-08-27 18:57:52 +0200

    Lister ut produktnavn

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import "./App.css";
 import products from "./data/products";
 
 function App() {
-
+  return (
+    <main>
+      <h1>Snackbar</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
