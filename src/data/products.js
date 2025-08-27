b732413dcee182993d1d07a1a5de364684085866
melz3r <ola.letrud@gmail.com>
2025-08-27 18:45:04 +0200

    lagt til produkter i products.js

---
+++ src/data/products.js
@@ -0,0 +1,53 @@
+const products = [
+  {
+    id: 1,
+    name: "Hamburger",
+    price: 110,
+    category: "burger",
+    image: "/hamburger.png",
+  },
+  {
+    id: 2,
+    name: "Luksushamburger",
+    price: 150,
+    category: "burger",
+    image: "/luksus-hamburger.png",
+  },
+  {
+    id: 3,
+    name: "Pølse",
+    price: 90,
+    category: "hotdog",
+    image: "/polse.png",
+  },
+  {
+    id: 4,
+    name: "Luksuspølse",
+    price: 120,
+    category: "hotdog",
+    image: "/luksus-polse.png",
+  },
+  {
+    id: 5,
+    name: "Coca-Cola",
+    price: 30,
+    category: "beverage",
+    image: "/coca-cola.png",
+  },
+  {
+    id: 6,
+    name: "Fanta",
+    price: 30,
+    category: "beverage",
+    image: "/fanta.png",
+  },
+  {
+    id: 7,
+    name: "Vann",
+    price: 30,
+    category: "beverage",
+    image: "/vann.png",
+  },
+];
+
+export default products;
