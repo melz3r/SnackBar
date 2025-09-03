277d827d6109b55932ec2575761aab2b78605384
melz3r <ola.letrud@gmail.com>
2025-09-03 20:44:00 +0200

    laget funksjon for å legge til item i cart og viser Cart i UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,56 @@ import "./App.css";
 import products from "./data/products";
 import ProductCard from "./components/ProductCard";
 import { useState } from "react";
+import Cart from "./components/Cart";
 
 export default function App() {
   const [filter, setFilter] = useState("all");
+  const [cart, setCart] = useState([]);
+
+  // filtrering
 
   const filteredProducts =
     filter === "all" ? products : products.filter((p) => p.category === filter);
 
+  // legg til i handlekurv
+
+function addToCart(product) {
+  setCart((prev) => {
+    const found = prev.find((item) => item.id === product.id);
+    return found
+      ? prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      : [...prev, { ...product, quantity: 1 }];
+  });
+}
+
+  // summering av handlekurv
+
+  const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
   return (
-    <section className="product-section">
+    <main>
       <h1>Snackbar</h1>
 
-      {/* filtreringsknapper */}
       <div className="filter-bar">
         <button onClick={() => setFilter("all")}>Vis alle produkter</button>
         <button onClick={() => setFilter("burger")}>Hamburgere</button>
-        <button onClick={() => setFilter("hotdog")}>Pølser</button>
-        <button onClick={() => setFilter("beverage")}>Drikke</button>
+        <button onClick={() => setFilter("polse")}>Pølser</button>
+        <button onClick={() => setFilter("drikke")}>Drikke</button>
       </div>
 
-      {/* produkter */}
-      <ul className="product-list">
-        {filteredProducts.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </ul>
-    </section>
+      <section className="product-section">
+        <ul className="product-list">
+          {filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} onBuy={addToCart} />
+          ))}
+        </ul>
+      </section>
+
+      <Cart items={cart} total={total} />
+    </main>
   );
 }
