b8e03c9f9df2d6501c3c23e9654e9ae97a456a59
melz3r <ola.letrud@gmail.com>
2025-09-03 20:23:03 +0200

    lagt til kjøpeknapp med onClick i ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,16 @@
-export default function ProductCard({ product }) {
+export default function ProductCard({ product, onBuy }) {
   return (
     <li className="product-card">
       <img src={product.image} alt={product.name} className="product-image" />
       <div className="product-info">
         <span className="product-name">{product.name}</span>
-        <span className="product-price">Legg til for kr {product.price},-</span>
+        <span className="product-price">
+          Kun {product.price} kr!
+        </span>
       </div>
+      <button className="buy-button" onClick={() => onBuy(product)}>
+        Legg til
+      </button>
     </li>
   );
 }
