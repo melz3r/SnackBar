ca5a9059c3add9d8842aaa8b2082211e43c8d13e
melz3r <ola.letrud@gmail.com>
2025-09-03 20:06:30 +0200

    lagt til filtrering med useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import "./App.css";
 import products from "./data/products";
 import ProductCard from "./components/ProductCard";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [filter, setFilter] = useState("all");
+
+  const filteredProducts =
+    filter === "all" ? products : products.filter((p) => p.category === filter);
+
   return (
     <section className="product-section">
       <h1>Snackbar</h1>
+
+      {/* filtreringsknapper */}
+      <div className="filter-bar">
+        <button onClick={() => setFilter("all")}>Vis alle produkter</button>
+        <button onClick={() => setFilter("burger")}>Hamburgere</button>
+        <button onClick={() => setFilter("hotdog")}>Pølser</button>
+        <button onClick={() => setFilter("beverage")}>Drikke</button>
+      </div>
+
+      {/* produkter */}
       <ul className="product-list">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </ul>
     </section>
   );
 }
-
-export default App;
